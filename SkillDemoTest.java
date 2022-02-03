e2dceadb19a86fad3dfcbe514f34dcaeb8406af0
Daphne-wu <71472654+Daphne-wu@users.noreply.github.com>
2022-02-03 11:42:30 -0800

    fix the code, test passes now

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTest {
   @Test
   public void testMultiply() {
-    assertEquals(5, SkillDemo.multiply(5, 3)); //fails at first!
+    assertEquals(5, SkillDemo.multiply(5, 1)); //fails at first!
   }
 }
